79996e7a62c2c8b6b3b20d58b338a097959e9316
Nate <7nater.nelson@gmail.com>
2021-01-26 08:44:14 -0700

    commit tree 51c1bda38ab8792a0d2c69e9f6f8a7accf91dd54

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
-  # code goes here
+  @@all = []
+  attr_accessor :owner, :mood
+  attr_reader :name
+  def initialize(name,owner)
+    @name = name
+    self.owner = owner
+    self.mood = "nervous"
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,65 @@
+require 'pry'
 class Owner
-  # code goes here
+  @@all = []
+  attr_reader :name, :species
+  def initialize(name)
+    @name = name
+    @species = "human"
+    self.class.all << self
+  end
+
+  def say_species
+    "I am a human."
+  end
+
+  def self.all
+    @@all 
+  end
+  def self.count
+    self.all.count
+  end
+  def self.reset_all
+    @@all.clear
+  end
+
+
+  def cats
+    Cat.all.select {|cat|cat.owner == self}
+  end
+  def dogs
+    Dog.all.select {|dog|dog.owner == self}
+  end
+  def buy_cat(cat_str)
+    # Cat.all.find {|cat|cat.name == cat_str}.owner = self
+    Cat.new(cat_str,self)
+  end
+
+  def buy_dog(dog_str)
+    # dog = Dog.all.find {|dog|dog.name == dog_str} != nil ? Dog.all.find {|dog|dog.name == dog_str} : 
+    Dog.new(dog_str,self) 
+    # dog.owner = self
+    # # binding.pry
+  end
+
+  def walk_dogs
+    dogs.each {|dog|dog.mood = "happy"}
+  end
+
+  def feed_cats
+    cats.each {|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    pets = [self.dogs,self.cats]
+    pets.each do |animal|
+      animal.each do|pet|
+        pet.owner = nil
+        pet.mood = "nervous"
+      end
+    end
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
 end
